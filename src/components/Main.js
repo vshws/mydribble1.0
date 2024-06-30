bc239f06305764b4f397f95fcc32c453f12cc3f2
vshws <93604055+vshws@users.noreply.github.com>
2024-06-30 16:02:15 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main() {
   return (
     <div className='relative h-[480px]'>
        <div className='absolute z-20 flex h-full flex-col items-start justify-center space-y-4 ml-12 text-white font-bold w-[800px]'>
-         <h2 className='text-4xl lg:text-6xl'>Discove the world's top designer and creative</h2>
+         <h2 className='text-6xl'>Discove the world's top designer and creative</h2>
          <p>
          The world’s leading brands use Dribbble to hire creative talent. Browse millions of top-rated portfolios to find your perfect creative match.
          </p>
