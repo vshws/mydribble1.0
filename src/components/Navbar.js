c311827cd8fef3715f0e94163df28af72021d080
vshws <93604055+vshws@users.noreply.github.com>
2024-06-30 15:43:37 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { SearchIcon } from "@heroicons/react/outline";
 
 function Navbar() {
     return (
-        <nav className='flex justify-between p-6 h-20'>
-            <div className='flex item-center w-[740px]'>
+        <nav className='flex flex-col lg:lex-row justify-between p-6 lg:h-20'>
+            <div className='flex item-center lg:w-2/3'>
                 <h2 className='font-bold text-xl'>dribble</h2>
-                <ul className='flex flex-1 justify-between ml-8 text-[14px] text-gray-600 font-bold'>
+                <ul className='hidden lg:flex flex-1 justify-between ml-8 text-sm text-gray-600 font-bold'>
                     <li>Inspiration</li>
                     <li>Find Work</li>
                     <li>Learn Design</li>
@@ -15,10 +15,10 @@ function Navbar() {
                     <li>Hire Designerd</li>
                 </ul>
             </div>
-            <div className='flex items-center justify-evenly w-[28%] text-gray-600'>
-                <SearchIcon className="h-4" />
-                <h3>Sign in ? Sign up</h3>
-                <button className='bg-pink-500 px-[12px] py-[9px] text-[15px] font-bold text-white rounded-md hover:bg-pink-400'>Start a free project </button>
+            <div className='flex items-center justify-end w-[28%] lg:w-1/3'>
+                <SearchIcon className="h-4 mr-4 lg:hidden" />
+                <h3 className='hidden lg:block'>Sign in ? Sign up</h3>
+                <button className='bg-pink-500 px-4 py-2 text-sm font-bold text-white rounded-md hover:bg-pink-400 ml-4 lg:ml-8'>Start a free project </button>
             </div>
 
 
