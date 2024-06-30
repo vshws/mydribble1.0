01da3c86344695c3484cdadd6e35ba6c4e6c825c
vshws <93604055+vshws@users.noreply.github.com>
2024-06-30 19:07:19 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,29 @@ import { SearchIcon } from "@heroicons/react/outline";
 
 function Navbar() {
     return (
-        <nav className='flex justify-between p-6 h-20'>
-            <div className='flex item-center w-[740px]'>
+        <nav className='flex flex-col lg:flex-row lg:justify-between lg:items-center p-6 lg:h-20'>
+            <div className='flex items-center lg:w-[740px]'>
                 <h2 className='font-bold text-xl'>dribble</h2>
-                <ul className='flex flex-1 justify-between ml-8 text-[14px] text-gray-600 font-bold'>
-                    <li>Inspiration</li>
-                    <li>Find Work</li>
-                    <li>Learn Design</li>
-                    <li>Go Pro</li>
-                    <li>Marketplace</li>
-                    <li>Hire Designerd</li>
+                <ul className='flex flex-wrap items-center justify-center lg:justify-start ml-0 lg:ml-8 text-sm lg:text-base text-gray-600 font-bold'>
+                    <li className='mx-1 lg:mx-2'>Inspiration</li>
+                    <li className='mx-1 lg:mx-2'>Find Work</li>
+                    <li className='mx-1 lg:mx-2'>Learn Design</li>
+                    <li className='mx-1 lg:mx-2'>Go Pro</li>
+                    <li className='mx-1 lg:mx-2'>Marketplace</li>
+                    <li className='mx-1 lg:mx-2'>Hire Designers</li>
                 </ul>
             </div>
-            <div className='flex items-center justify-evenly w-[28%] text-gray-600'>
-                <SearchIcon className="h-4" />
-                <h3>Sign in ? Sign up</h3>
-                <button className='bg-pink-500 px-[12px] py-[9px] text-[15px] font-bold text-white rounded-md hover:bg-pink-400'>Start a free project </button>
+            
+         
+            <div className='flex justify-between items-center w-full lg:w-auto mt-4 lg:mt-0'>
+                <div className='flex items-center w-full lg:w-[28%]'>
+                    <SearchIcon className="h-4 mr-2 " /> 
+                    <h3 className='text-gray-600'>Sign in / Sign up</h3>
+                </div>
+                <button className='bg-pink-500 px-4 py-2 text-[15px] font-bold text-white rounded-md lg:ml-4 hover:bg-pink-400'>
+                    Start a free project
+                </button>
             </div>
-
-
         </nav>
     );
 }
