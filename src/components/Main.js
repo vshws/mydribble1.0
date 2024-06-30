a70933a63f7a7e8aa4419be31cda470ccf3fbeff
vshws <93604055+vshws@users.noreply.github.com>
2024-06-30 21:55:04 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ import img1 from '../img/img1.jpg'
 function Main() {
   return (
     <div className='relative h-[480px]'>
-       <div className='absolute z-20 flex h-full flex-col items-start justify-center space-y-4 ml-12 text-white font-bold w-[800px]'>
+       <div className='absolute z-20 flex h-full flex-col flex-row items-start justify-center space-y-4 ml-12 text-white font-bold w-[900px]'>
          <h2 className='text-6xl'>Discove the world's top designer and creative</h2>
          <p>
          The world’s leading brands use Dribbble to hire creative talent. Browse millions of top-rated portfolios to find your perfect creative match.
