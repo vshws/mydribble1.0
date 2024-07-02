a1a81d2d726982eb606a6d4d0ed66927f08834e5
vshws <93604055+vshws@users.noreply.github.com>
2024-07-02 23:40:52 +0530

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -24,18 +24,10 @@ function Project({ name, team, likes, views, img }) {
   };
 
   return (
-    <div
-      className='w-full h-[320px] rounded-xl mb-5 shadow-lg'
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className='w-full h-[320px] rounded-xl mb-5 shadow-lg' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
       <div className='relative w-full h-[320px] overflow-hidden rounded-t-xl'>
-        {/* Using an img tag to fill the div */}
-        <img
-          src={img}
-          alt={name}
-          className='absolute inset-0 w-full h-full object-cover rounded-t-xl'
-        />
+
+    <img src={img} alt={name} className='absolute inset-0 w-full h-full object-cover rounded-t-xl'/>
         {isHovered && (
           <div className='absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
             <div className='text-white text-center'>
@@ -55,21 +47,6 @@ function Project({ name, team, likes, views, img }) {
           </div>
         )}
       </div>
-      // <div className='p-4'>
-      //   <div className='flex items-center'>
-      //     <h3 className='text-[15px] text-black font-bold'>{name}</h3>
-      //     <h3 className='bg-gray-300 text-white p-[2px] text-xs font-bold rounded-md ml-2'>{team}</h3>
-      //   </div>
-      //   <div className='flex space-x-2 mt-2'>
-      //     <div className='flex items-center cursor-pointer' onClick={handleLikeClick}>
-      //       <HeartIcon className={`h-5 w-5 ${isLiked ? 'text-red-500' : 'text-gray-500'}`} />
-      //       <h2 className='ml-1'>{currentLikes}</h2>
-      //     </div>
-      //     <div className='flex items-center'>
-      //       <EyeIcon className='h-5 w-5 text-gray-500' />
-      //       <h2 className='text-sm'>{views}</h2>
-      //     </div>
-      //   </div>
       </div>
     </div>
   );
